0dd2bed6fd9db55f6b9561a4d015f71378e2a543
Leigh Pauls <leighpauls@fb.com>
2016-03-16 13:30:20 -0700

    open compilation errors in intellij

--- .emacs.d/leighpauls/leigh-compilation.el
+++ .emacs.d/leighpauls/leigh-compilation.el
@@ -31,4 +31,22 @@
   (toggle-read-only))
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
+(defun compile-goto-error-in-idea ()
+  "Visit the source for the error in Intellij Idea"
+  (interactive)
+  (or (compilation-buffer-p (current-buffer))
+      (error "Not in a compilation beginning"))
+  (save-excursion
+    (beginning-of-line)
+    (let ((beg (point)))
+      (end-of-line)
+      (let ((str (buffer-substring-no-properties beg (point))))
+        (string-match "^\\(.+?\\):\\(.+?\\):" str)
+        (shell-command (format "idea --line %s %s"
+                               (match-string 2 str)
+                               (match-string 1 str)))))))
+
+(add-hook 'compilation-mode-hook
+          (lambda () (local-set-key (kbd "i") 'compile-goto-error-in-idea)))
+
 (provide 'leigh-compilation)
