09570a1476a2070424c4d35878dc16a9efe03b22
Leigh Pauls <leighpauls@fb.com>
2016-07-15 11:25:17 -0700

    Remove highlight 80+ mode

--- .emacs.d/leighpauls/leigh-load-modes.el
+++ .emacs.d/leighpauls/leigh-load-modes.el
@@ -17,10 +17,10 @@
 (require 'camelCase-mode)
 (add-hook 'javascript-mode-hook '(lambda () (camelCase-mode 1)))
 
-(add-to-list 'load-path "~/.emacs.d/highlight-80+/")
-(require 'highlight-80+)
-(add-hook 'python-mode-hook 'highlight-80+-mode)
-(add-hook 'c-mode-common-hook 'highlight-80+-mode)
+;; (add-to-list 'load-path "~/.emacs.d/highlight-80+/")
+;; (require 'highlight-80+)
+;; (add-hook 'python-mode-hook 'highlight-80+-mode)
+;; (add-hook 'c-mode-common-hook 'highlight-80+-mode)
 
 (add-to-list 'load-path "~/.emacs.d/android-mode/")
 (require 'android-mode)
