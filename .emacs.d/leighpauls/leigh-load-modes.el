4bd8191de4e604d2de018f579b6e8540b53e81c2
Leigh Pauls <leigh@nextdoor.com>
2019-09-06 10:56:10 -0700

    update rust-mode

--- .emacs.d/leighpauls/leigh-load-modes.el
+++ .emacs.d/leighpauls/leigh-load-modes.el
@@ -30,7 +30,7 @@
 (require 'monky)
 (setq monky-process-type 'cmdserver)
 
-(add-to-list 'load-path "~/.emacs.d/rust-mode-0.3.0/")
+(add-to-list 'load-path "~/.emacs.d/rust-mode-0.4.0/")
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
