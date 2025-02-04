edd7dac5234ff5764f96e964b146f0cf52db03a6
Leigh Pauls <lpauls@roblox.com>
2025-02-04 11:29:10 -0800

    refresh elpa

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -1,6 +1,16 @@
-((magit-branch nil)
+((magit-blame
+  ("-w"))
+ (magit-branch nil)
  (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-fetch nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
  (magit-pull nil)
- (magit-push nil))
+ (magit-push
+  ("--force-with-lease"))
+ (magit-rebase
+  ("--autostash")
+  ("--autostash" "--interactive")
+  nil)
+ (magit-stash nil))
